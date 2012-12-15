373a61547c7c52f1bd1838ae3b25f4a270421121
Jen Fong-Adwent <jen@ednapiranha.com>
2012-12-15 14:36:50 -0500

    basic navigation support; default internal layouts

---
+++ js/lib/settings.js
@@ -0,0 +1,86 @@
+'use strict';
+
+define(['jquery'],
+  function($) {
+
+  var land = $('#game-area');
+  var timeOfDay = $('#time-of-day');
+  var scene = $('#scene-change');
+  var robot = $('#robot-bev');
+  var currentLocation = 'all';
+
+  var loadLocation = function(location) {
+    localStorage.setItem('mechanicalError-location', location);
+    scene.addClass('on');
+    console.log(location)
+    setTimeout(function() {
+      if (location === 'tree1' || location === 'tree2' || location === 'tree3') {
+        currentLocation = 'tree';
+        land
+          .removeClass()
+          .addClass('tree');
+      } else if (location === 'house') {
+        currentLocation = 'house';
+        land
+          .removeClass()
+          .addClass('house');
+      } else {
+        currentLocation = 'all';
+        land.removeClass();
+        self.setWeather();
+      }
+      robot.css({ left: '0', top: '0' });
+      scene.removeClass();
+    }, 2000);
+  };
+
+  var self = {
+    setWeather: function() {
+      var currSeason = (new Date()).getMonth();
+
+      land.removeClass();
+
+      if (currSeason > 2 && currSeason < 7) {
+        land.addClass('spring');
+      } else if (currSeason > 6 && currSeason < 9) {
+        land.addClass('summer');
+      } else if (currSeason > 8 && currSeason < 11) {
+        land.addClass('autumn');
+      } else {
+        land.addClass('winter');
+      }
+    },
+
+    setTimeOfDay: function() {
+      var currTime = (new Date()).getHours();
+
+      timeOfDay.removeClass();
+
+      if (currTime > 16 && currTime < 21) {
+        timeOfDay.addClass('evening');
+      } else if ((currTime > 20 && currTime < 24) || (currTime > -1 && currTime < 6)) {
+        timeOfDay.addClass('night');
+      }
+    },
+
+    setTarget: function(left, top) {
+      // House
+      console.log(left, top)
+      if (left > 225 && top > 390) {
+        if (currentLocation === 'tree' || currentLocation === 'house') {
+          loadLocation('all');
+        } else {
+          loadLocation('house');
+        }
+      } else if (left > 68 && left < 97 && top > 210 && top < 250) {
+        loadLocation('tree1');
+      } else if (left > 159 && left < 185 && top > 197 && top < 230) {
+        loadLocation('tree2');
+      } else if (left > 139 && left < 165 && top > 278 && top < 312) {
+        loadLocation('tree3');
+      }
+    }
+  };
+
+  return self;
+});
--- js/main.js
+++ js/main.js
@@ -5,13 +5,19 @@ requirejs.config({
   enforceDefine: true
 });
 
-define(['jquery'],
-  function($) {
+define(['jquery', 'settings'],
+  function($, settings) {
 
   var game = $('#game-wrapper');
   var robot = $('#robot-bev');
 
+  var MOVE_SPEED = 2000;
+
+  settings.setWeather();
+  settings.setTimeOfDay();
+
   game.on('click', '#game-area', function(ev) {
+    var target = $(ev.target);
     var currLeft = ev.pageX;
     var currTop = ev.pageY;
 
@@ -30,9 +36,23 @@ define(['jquery'],
     if (currTop > 450) {
       currTop = 450;
     }
-    robot.animate({
-      left: currLeft + 'px',
-      top: currTop + 'px'
-    }, 2000);
+
+    if (!target.is('#robot-bev')) {
+      var speed = MOVE_SPEED;
+
+      var travelDiffLeft = Math.abs(currLeft - robot.position().left);
+      var travelDiffTop = Math.abs(currTop - robot.position().top);
+
+      if (travelDiffLeft < 40 || travelDiffTop < 40) {
+        speed = MOVE_SPEED / 2;
+      }
+
+      robot.animate({
+        left: currLeft + 'px',
+        top: currTop + 'px'
+      }, speed, function() {
+        settings.setTarget(currLeft, currTop);
+      });
+    }
   });
 });
